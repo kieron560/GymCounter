8fae47e8f98512333045a997388f9d1acf6ecab4
kieron560 <kieron560@gmail.com>
2021-06-23 04:19:33 +0800

    Update Bot.py

--- bot.py
+++ bot.py
@@ -75,7 +75,7 @@ def main():
                           port=int(PORT),
                           url_path=TOKEN)
     updater.bot.setWebhook('https://protected-beach-15254.herokuapp.com/' + TOKEN)
-
+    # updater.start_polling()
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
