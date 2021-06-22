487761a2e599484d37d576321ee709d66f19b667
kieron560 <kieron560@gmail.com>
2021-06-23 04:04:58 +0800

    Draft

--- main.py
+++
@@ -1,5 +0,0 @@
-import telegram
-
-bot = telegram.Bot(token='1726981118:AAHqAMtbZdZRLwTdo1DED0LDD6h8PUBvNNw')
-
-print(bot.get_me())
--- scrapper.py
+++ scrapper.py
@@ -3,9 +3,16 @@ import re
 import ast
 from bs4 import BeautifulSoup
 
-LINK = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSW5uZLrFwbB16BFBvn7XaIwlsgudMwMmHlqSOs8I5etYAYuek6jgYEQM4cDvhRCDWp5a1ZGLwHLkkn/pubchart?oid=291919261&format=interactive"
+LINK_ELIAS = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSW5uZLrFwbB16BFBvn7XaIwlsgudMwMmHlqSOs8I5etYAYuek6jgYEQM4cDvhRCDWp5a1ZGLwHLkkn/pubchart?oid=291919261&format=interactive"
+LINK_EHUB = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQKl9d-Qyig483T0HB8Y8C3yKngbXny3uXzjr6YhAPQmtHTGqAHo727SEOHbnOsKNj9c330vaCL3VOU/pubchart?oid=291919261&format=interactive"
+LINK = ""
 
-def get_info():
+def get_info(choice):
+    if choice == 1:
+        LINK = LINK_ELIAS
+    else:
+        LINK = LINK_EHUB
+
     r = requests.get(LINK)
     soup = BeautifulSoup(r.content, 'html.parser') 
     info = soup.find("script").find_next_sibling()
@@ -15,9 +22,10 @@ def get_info():
 
     dictionary = ast.literal_eval(converted)
 
-    location = dictionary['options']['title']
-    time = dictionary['subtitle']
+    location = dictionary['options']['title'][20:]
+    time = dictionary['options']['subtitle'][-7:]
     percentage = dictionary['dataTable']['rows'][0]['c'][0]['f']
     waiting = dictionary['dataTable']['rows'][0]['c'][1]['f'] 
 
     return location, time, percentage, waiting
+
