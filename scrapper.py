0139407f8d0d0fac91d146f2cb0b522e2b9a2a0a
kieron560 <kieron560@gmail.com>
2021-06-23 03:25:03 +0800

    Successfully parsed information fully

--- scrapper.py
+++ scrapper.py
@@ -5,12 +5,19 @@ from bs4 import BeautifulSoup
 
 LINK = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSW5uZLrFwbB16BFBvn7XaIwlsgudMwMmHlqSOs8I5etYAYuek6jgYEQM4cDvhRCDWp5a1ZGLwHLkkn/pubchart?oid=291919261&format=interactive"
 
-r = requests.get(LINK)
-soup = BeautifulSoup(r.content, 'html.parser') 
-info = soup.find("script").find_next_sibling()
+def get_info():
+    r = requests.get(LINK)
+    soup = BeautifulSoup(r.content, 'html.parser') 
+    info = soup.find("script").find_next_sibling()
 
-trimmed = re.search("'chartJson': '(.*?)', 'serializedChartProperties'", str(info)).group(1)
-converted = "\"" + trimmed.replace("\\/", "/").encode().decode('unicode_escape').replace('\"','\'') + "\""
-# x = ast.literal_eval(y)
-# print(x)
-# print(type(x))
+    trimmed = re.search("'chartJson': '(.*?)', 'serializedChartProperties'", str(info)).group(1)
+    converted =  trimmed.replace("\\/", "/").encode().decode('unicode_escape').replace('\"','\'').replace("true", "True")
+
+    dictionary = ast.literal_eval(converted)
+
+    location = dictionary['options']['title']
+    time = dictionary['subtitle']
+    percentage = dictionary['dataTable']['rows'][0]['c'][0]['f']
+    waiting = dictionary['dataTable']['rows'][0]['c'][1]['f'] 
+
+    return location, time, percentage, waiting
